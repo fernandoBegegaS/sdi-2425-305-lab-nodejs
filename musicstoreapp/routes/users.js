2c57a2d97df5af59199043ab923095c49a38f41e
Fernando <uo295286@uniovi.es>
2025-03-24 21:54:07 +0100

    SDI-2425-305-10.3-Mensajes y alertas.

--- musicstoreapp/routes/users.js
+++ musicstoreapp/routes/users.js
@@ -14,13 +14,13 @@ module.exports = function (app, usersRepository) {
       email: req.body.email,
       password: securePassword
     }
-    usersRepository.insertUser(user).then(userId => {
-      res.send('Usuario registrado ' + userId);
-    }).catch(error => {
-      res.send("Error al insertar el usuario");
-    });
 
-  })
+    usersRepository.insertUser(user).then(userId => {
+      res.redirect('/users/login' + "?message=Nuevo usuario registrado." + "&messageType=alert-info");
+    }).catch(error => {
+      res.redirect('/users/signup' + "?message=Se ha producido un error al registrar el usuario." + "&messageType=alert-danger");
+    });
+  });
 
   app.get('/users/login', function (req, res) {
     res.render("login.twig");
@@ -32,34 +32,32 @@ module.exports = function (app, usersRepository) {
   })
 
   app.post('/users/login', function (req, res) {
-    let securePassword = app.get('crypto')
-        .createHmac('sha256', app.get('clave'))
-        .update(req.body.password)
-        .digest('hex');
-
+    let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
+        .update(req.body.password).digest('hex');
     let filter = {
       email: req.body.email,
       password: securePassword
-    };
-
+    }
     let options = {};
-
-    usersRepository.findUser(filter, options)
-        .then(user => {
-          if (user == null) {
-            req.session.user = null;
-            res.send("Usuario no identificado");
-          } else {
-            req.session.user = user.email;
-
-            res.redirect("/publications");
-
-           }
-        })
-        .catch(error => {
-          req.session.user = null;
-          res.send("Se ha producido un error al buscar el usuario: " + error);
-        });
+    usersRepository.findUser(filter, options).then(user => {
+      if (user == null) {
+        req.session.user = null;
+        //res.send("Usuario no identificado");
+        res.redirect("/users/login" +
+            "?message=Email o password incorrecto"+
+            "&messageType=alert-danger ");
+      } else {
+        req.session.user = user.email;
+        //res.send("Usuario identificado correctamente: " + user.email
+        res.redirect("/publications");
+      }
+    }).catch(error => {
+      req.session.user = null;
+      //res.send("Se ha producido un error al buscar el usuario: " + error)
+      res.redirect("/users/login" +
+          "?message=Se ha producido un error al buscar el usuario"+
+          "&messageType=alert-danger ");
+    })
   });
 }
 
