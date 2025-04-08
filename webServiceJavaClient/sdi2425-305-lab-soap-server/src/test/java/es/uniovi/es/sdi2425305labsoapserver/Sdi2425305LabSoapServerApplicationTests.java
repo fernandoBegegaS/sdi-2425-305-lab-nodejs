e32d5b792001b8dc7d6a7dbd02b7cdb64f8867b3
Fernando <uo295286@uniovi.es>
2025-04-08 13:32:16 +0200

    SDI-2425-305-SW-11.1-Servicio-SOAP-Server.

---
+++ webServiceJavaClient/sdi2425-305-lab-soap-server/src/test/java/es/uniovi/es/sdi2425305labsoapserver/Sdi2425305LabSoapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uniovi.es.sdi2425305labsoapserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2425305LabSoapServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
